1098dc2c24c39ad39c59de8c4b1143d6b075533f
Jermin-kyle <perezjermainekyle@gmail.com>
2026-08-13 23:05:07 +0800

    commit

--- app.py
+++ app.py
@@ -59,4 +59,8 @@ def redirect_short(short_code):
     return "Short URL not found", 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(
+        host='0.0.0.0',
+        port=5000,
+        debug=True
+    )
